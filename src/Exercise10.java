fe145b363b107f995ca006ef9636c6b3d4200067
Michael Staunton <michael@hilltopassociates.com>
2020-10-11 21:09:08 -0400

    final commit

--- src/Exercise10.java
+++ src/Exercise10.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Exercise10 {
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercise10 {
+    public static void main(String[] args) {
+        DecimalFormat Decimal = new DecimalFormat("#.00");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter wage: ");
+        double Wage = in.nextDouble();
+        System.out.print("Enter hours worked: ");
+        double Hours = in.nextDouble();
+        double Overtime =Hours - 40;
+        Overtime = Overtime*Wage*1.5;
+        double MaxRegularHours = Wage*40;
+        double Earnings = 0;
+
+        System.out.println();
+
+        if (Hours>40) {
+            Earnings = Overtime+MaxRegularHours;
+        } else if (Hours<=40) {
+            Earnings = Wage*Hours;
+        }
+
+        if(Hours<0){
+            System.out.println("Your hours must be greater than or equal to 0.0.");
+        } else if(Wage<0){
+            System.out.println("Your wage must be greater than or equal to $0.00/hour");
+        } else{
+            System.out.println("You'll make $"+Earnings+" this week.");
+        }
+
+        System.out.println();
+    }
 }
--- src/Exercise8.java
+++ src/Exercise8.java
@@ -4,14 +4,11 @@ public class Exercise8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a temperature: ");
-        String Temperature = in.nextLine();
-        int Length = Temperature.length();
-        String Temp = Temperature.substring(0,Length);
-        Double Value = Double.valueOf(Temp);
+        int Value = in.nextInt();
+        String Measure = in.next();
         String Matter = "";
 
-        if(TemperatureScale.contains("F")||TemperatureScale.contains("f")){
-            Temperature.indexOf(F)
+        if(Measure.contains("F")||Measure.contains("f")){
             if(Value >32 && Value <= 212){
                 Matter = "Liquid.";
             }else if(Value <= 32){
@@ -20,7 +17,7 @@ public class Exercise8 {
                 Matter = "Gas.";
             }
 
-        } else if (TemperatureScale.contains("C")||TemperatureScale.contains("c")){
+        } else if (Measure.contains("C")||Measure.contains("c")){
             if(Value <= 0){
                 Matter = "Solid.";
             } else if(Value > 0 && Value <=100){
